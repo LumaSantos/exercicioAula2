828905eddcbcf75c16cbd35ce92dc2f85da0c3dc
LumaSantos <63914574+LumaSantos@users.noreply.github.com>
2024-10-16 22:49:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+var nomeInserido = prompt("Digite seu nome");
+var sobrenomeInserido = prompt("Digite seu sobrenome");
+var idade = prompt("Digite sua idade");
+var saída = nomeInserido + sobrenomeInserido + idade;
+alert(nomeInserido + " " + sobrenomeInserido + ", " + idade);
